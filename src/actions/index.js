3c7a5fb86c63f733ea5bfcd62e650ae66121be86
mattia lugaro <mattialugaro@gmail.com>
2024-01-16 17:26:39 +0100

    da finire

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ export const increment = () => ({ type: 'INCREMENT' });
 export const decrement = () => ({ type: 'DECREMENT' });
 
 export const setUser = (user) => ({ type: 'SET_USER', payload: user });
-export const clearUser = () => ({ type: 'CLEAR_USER' })
+export const clearUser = () => ({ type: 'CLEAR_USER' });
 
 export const setCart = (cart) => ({ type: 'SET_CART', payload: cart});
 export const clearCart = () => ({ type: 'CLEAR_CART' });
